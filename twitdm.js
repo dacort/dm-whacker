457db8db5f0701a49d8d6ee90f1a80e879f4c916
Damon P. Cortesi <d.lifehacker@gmail.com>
2009-11-22 20:50:59 -0800

    Updated to address changes in Twitter's UI/Ajax

--- twitdm.js
+++ twitdm.js
@@ -5,14 +5,13 @@
 // ==/UserScript==
 (function(){
 
-	var Version = '0.6.1';
-	var lastUpdate = '2009.05.01';
+	var Version = '0.6.2-gobblegobble';
+	var lastUpdate = '2009.11.22';
 	var scriptURL = 'http://dcortesi.com/dm_deleter/twitdm_dev.js';
 	var scriptText = "javascript:(function(){if(!location.href.match(/http:\/\/twitter.com\/direct_messages/) && !location.href.match(/timeline\/direct_messages$/) && !location.href.match(/http:\/\/twitter.com\#inbox/)){if(confirm('You%20must%20be%20on%20the%20Twitter%20direct%20messages%20page.\nWould%20you%20like%20me%20to%20take%20you%20there?')){location.href='http://twitter.com/direct_messages';};return%20false;};var%20s%20=%20document.createElement('script');s.charset='utf-8';s.type='text/javascript';s.src='" + scriptURL + "';document.body.appendChild(s);})();void(0);";
 
-    var side = document.getElementById('side');
-    if(side == null) return;
-    side.innerHTML = '<div class="section" style="margin-bottom:20px"><B>Twitter DM Deleter</B><br/><div style="margin-left:10px;color:#666666;margin-bottom:10px;">version:'+Version+'<br/>last update:'+lastUpdate+'</div><b>Select DM\'s to Delete</b><br/><form id="frm_delete" name="frm_delete"><input id="delete_dm_all" type="radio" value="all" name="delete_dm_type" checked="checked"/> <label for="delete_dm_all">all dm\'s</label><br /><input id="delete_dm_user" type="radio" value="user" name="delete_dm_type" /> <label for="delete_dm_user">dm\'s with user:</label><input onfocus="$(\'#delete_dm_user\').attr(\'checked\',true)" id="delete_dm_username" type="text" name="delete_dm_username" /><br /><br /><input id="delete_dm_inbox" type="checkbox" value="delete_inbox" name="delete_dm_inbox" checked="true" /> <label for="delete_dm_inbox">include Inbox items</label><br /><input id="delete_dm_sent" type="checkbox" value="delete_sent" name="delete_dm_sent" /> <label for="delete_dm_sent">include Sent items</label><br /><br /><a href="#" id="advlink" onclick="$(\'#advanced_feat\').slideToggle(\'normal\',function(){$(\'#advlink\')[0].innerHTML = ($(\'#advlink\')[0].innerHTML == \'+ Advanced Features\'? \'- Advanced Features\':\'+ Advanced Features\')})">+ Advanced Features</a><div id="advanced_feat" style="display: none;"><br /><input id="delete_dm_match" type="checkbox" value="match" name="delete_dm_match" /> <label for="delete_dm_match">dm\'s containing text:</label><input onfocus="$(\'#delete_dm_match\').attr(\'checked\',true)" id="delete_dm_matchtext" type="text" name="delete_dm_matchtext" /><br /><input id="match_type_simple" type="radio" value="simple" name="match_type" checked="checked"/> <label for="match_type_simple">Simple</label> <input id="match_type_regex" type="radio" value="regex" name="match_type" onclick="if (!confirm(\'Regular expressions are intended for advanced users. Please click OK only if you know what you are doing. :)\\n\\nPlease note, all searches are currently NOT case-sensitive.\')) {$(\'#match_type_simple\').attr(\'checked\', true);}" /> <label for="match_type_regex">RegEx Power!</label></div><br /><br /><input type="button" id="delete_dm_submit" value="Delete!" /><input type="hidden" id="delete_dm_count_inbox" value="0"/><input type="hidden" id="delete_dm_count_sent" value="0"/><br /><br /></form></div>' + side.innerHTML;
+    var sideForm = '<div class="section" style="margin-bottom:20px"><B>Twitter DM Deleter</B><br/><div style="margin-left:10px;color:#666666;margin-bottom:10px;">version:'+Version+'<br/>last update:'+lastUpdate+'</div><b>Select DM\'s to Delete</b><br/><form id="frm_delete" name="frm_delete"><input id="delete_dm_all" type="radio" value="all" name="delete_dm_type" checked="checked"/> <label for="delete_dm_all">all dm\'s</label><br /><input id="delete_dm_user" type="radio" value="user" name="delete_dm_type" /> <label for="delete_dm_user">dm\'s with user:</label><input onfocus="$(\'#delete_dm_user\').attr(\'checked\',true)" id="delete_dm_username" type="text" name="delete_dm_username" /><br /><br /><input id="delete_dm_inbox" type="checkbox" value="delete_inbox" name="delete_dm_inbox" checked="true" /> <label for="delete_dm_inbox">include Inbox items</label><br /><input id="delete_dm_sent" type="checkbox" value="delete_sent" name="delete_dm_sent" /> <label for="delete_dm_sent">include Sent items</label><br /><br /><a href="#" id="advlink" onclick="$(\'#advanced_feat\').slideToggle(\'normal\',function(){$(\'#advlink\')[0].innerHTML = ($(\'#advlink\')[0].innerHTML == \'+ Advanced Features\'? \'- Advanced Features\':\'+ Advanced Features\')})">+ Advanced Features</a><div id="advanced_feat" style="display: none;"><br /><input id="delete_dm_match" type="checkbox" value="match" name="delete_dm_match" /> <label for="delete_dm_match">dm\'s containing text:</label><input onfocus="$(\'#delete_dm_match\').attr(\'checked\',true)" id="delete_dm_matchtext" type="text" name="delete_dm_matchtext" /><br /><input id="match_type_simple" type="radio" value="simple" name="match_type" checked="checked"/> <label for="match_type_simple">Simple</label> <input id="match_type_regex" type="radio" value="regex" name="match_type" onclick="if (!confirm(\'Regular expressions are intended for advanced users. Please click OK only if you know what you are doing. :)\\n\\nPlease note, all searches are currently NOT case-sensitive.\')) {$(\'#match_type_simple\').attr(\'checked\', true);}" /> <label for="match_type_regex">RegEx Power!</label></div><br /><br /><input type="button" id="delete_dm_submit" value="Delete!" /><input type="hidden" id="delete_dm_count_inbox" value="0"/><input type="hidden" id="delete_dm_count_sent" value="0"/><br /><br /></form></div>';
+    $("#side").prepend(sideForm);
     
     var deleteMessages = function(url) {
         // Function to delete messages in the, with the current_url passed as a parameter
@@ -29,7 +28,10 @@
         if ($("#timeline").length == 0 || $("li.direct_message").length == 0) {
             if ($('#delete_dm_sent').attr("checked") && (url.indexOf("/sent") < 0)) {
                 url = base_url + '/sent?page=1';
-                $("#timeline").load(url + " #timeline li", '', function(){deleteMessages(url)})
+                $.getJSON(url, '', function(data){
+                   $("#timeline").replaceWith(data["#timeline"]);
+                   deleteMessages(url);
+                 });
                 return;
             } else {
                 var del_inbox = $('#delete_dm_count_inbox').attr("value");
@@ -71,7 +73,7 @@
         // } else {
         //     document.getElementById('content').getElementsByTagName('h2')[0].parentNode.innerHTML = $("#timeline", twitter_data).parent().html();
         // }
-        var tweet_list = document.getElementById('timeline').getElementsByTagName('li');
+        var tweet_list = $("#timeline .direct_message");
         
         var messages_deleted = 0;
         var arr_deleted = [];
@@ -84,8 +86,8 @@
             
             // The status's numerical ID
             var dm_id = tweet_list[i].id.replace(/direct_message_/, '');
-            var link = '/direct_messages/destroy/' + dm_id
-            var token = twttr.form_authenticity_token
+            var link = '/direct_messages/destroy/' + dm_id;
+            var token = twttr.form_authenticity_token;
             
             // Determine if basic or advanced mode is selected and match appropriately
             var match = -1;
@@ -160,7 +162,10 @@
 
                 // Make sure we even have another page (TODO)
                 url = base_url + '?page='+current_page;
-                $("#timeline").load(url + " #timeline li", '', function(){deleteMessages(url)});
+                $.getJSON(url, '', function(data){
+                   $("#timeline").replaceWith(data["#timeline"]);
+                   deleteMessages(url);
+                 });
             }
         }
         
@@ -204,7 +209,10 @@
         }
         
         // Load our data into the current timeline
-        $("#timeline").load(url + " #timeline li", '', function(){deleteMessages(url)});
+        $.getJSON(url, '', function(data){
+          $("#timeline").replaceWith(data["#timeline"]);
+          deleteMessages(url);
+        });
 
     }
     // Now using jquery, one statement
